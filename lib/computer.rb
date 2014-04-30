f2dd667e844eab8597b47c126d34d2fb077a4c67
tesstryan <tesstryan@gmail.com>
2014-04-30 15:52:40 -0500

    Updated Computer Class

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,10 +1,21 @@
+require_relative 'guess_evaluator'
+
 class Computer
 
-  def make_guess
-    "RRRR"
+  attr_reader :number_of_guesses
+
+  def initialize
+    @number_of_guesses = 0
   end
 
-  def evaluates_feedback
+  def make_guess
+    @number_of_guesses += 1
+    guess = ["R", "G", "B", "O", "Y", "P"].repeated_permutation(4).to_a.sample(1).join
+
   end
+
+
 
 end
+
+
